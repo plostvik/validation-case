b5c1b8fd03cc9f4687cc782f5bf7bba60a0c41eb
plostvik <plostvik@gmail.com>
2023-08-12 16:18:45 +0300

    UC#8 React initial setup

---
+++ src/store/reducer.js
@@ -0,0 +1,22 @@
+import { SET_USER_INFO } from './actions';
+
+const initialState = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  message: '',
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER_INFO:
+      return {
+        ...state,
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
